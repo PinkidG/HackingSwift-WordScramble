8507f0e68cd269ba47e17a6c0868c1fba101e8c3
Philipp Pinkernelle <philipp.pinkernelle@sap.com>
2022-07-06 21:25:47 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Philipp Pinkernelle on 06.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
