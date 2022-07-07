a8e1cb33a284e0768c3dd64588e2c3ebb1b2a678
Philipp Pinkernelle <philipp.pinkernelle@sap.com>
2022-07-07 22:35:13 +0200

    adding words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,13 +8,138 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = "WordScramble"
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .onSubmit(addNewWord)
+                        .onAppear(perform: startGame)
+                        .alert(errorTitle, isPresented: $showingError) {
+                            Button("OK", role: .cancel) { }
+                        } message: {
+                            Text(errorMessage)
+                        }
+                }
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Your score is: \(score)")
+                }
+                ToolbarItem {
+                    
+                    Button("Restart") {
+                        startGame()
+                    }
+                }
+            }
         }
+    }
+    
+    private func addNewWord(){
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        guard answer != rootWord else {
+            wordError(title: "That's the root word!", message: "Try to find different words!")
+            return
+        }
+        
+        guard isLongEnough(word: answer) else {
+            wordError(title: "Word is too short", message: "Must be 3 characters at least.")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        score += answer.count
+        newWord = ""
+    }
+    
+    private func startGame(){
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsUrl){
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = [String]()
+                score = 0
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    private func isLongEnough(word: String) -> Bool{
+        return word.count > 3
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    private func isPossible(word: String) -> Bool{
+        var tmpWord = rootWord
+        
+        for letter in word{
+            if let pos = tmpWord.firstIndex(of: letter){
+                tmpWord.remove(at: pos)
+            }
+            else{
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    private func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
